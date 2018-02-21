1497ed553ef1cae95d9dfa95e2ee3b1c92a5639a
bretzel12 <bretzel12@users.noreply.github.com>
2018-02-20 21:40:28 -0800

    Document commands added.
    - Allows undo/redo framework to be used.

---
+++ include/on_sag_doc_commands.h
@@ -0,0 +1,155 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#ifndef OTLS_ONSAG_ONSAGDOCCOMMANDS_H_
+#define OTLS_ONSAG_ONSAGDOCCOMMANDS_H_
+
+#include <string>
+
+#include "wx/cmdproc.h"
+#include "wx/xml/xml.h"
+
+#include "on_sag_doc.h"
+
+/// \par OVERVIEW
+///
+/// This class is a command that modifies spans in the document.
+///
+/// \par NAME
+///
+/// The command must be initialized with one of the declared names, as it will
+/// determine what kind of action is to be performed.
+///
+/// \par SPAN XML NODES
+///
+/// This command stores sag span xml nodes. Up to two nodes are stored. One of
+/// the nodes will be committed to the document on a 'do' method, while the
+/// other will be committed to the document on an 'undo' method. These nodes
+/// may not be needed for all command types, and will be kept as a nullptr if
+/// not used.
+///
+/// \par COMMAND INFORMATION
+///
+/// The following information is needed for each command name/type:
+/// - Delete = index
+/// - Insert = index + span node
+/// - Modify = index + span node
+/// - Move Down = index
+/// - Move Up = index
+class SagSpanCommand : public wxCommand {
+ public:
+  /// \var kNameDelete
+  ///   The command string to use for deleting a span.
+  static const std::string kNameDelete;
+
+  /// \var kNameInsert
+  ///   The command name to use for inserting a span.
+  static const std::string kNameInsert;
+
+  /// \var kNameModify
+  ///   The command string to use for modifying a span.
+  static const std::string kNameModify;
+
+  /// \var kNameMoveDown
+  ///   The command string to use for moving a span down.
+  static const std::string kNameMoveDown;
+
+  /// \var kNameMoveUp
+  ///   The command string to use for moving a span up.
+  static const std::string kNameMoveUp;
+
+  /// \brief Constructor.
+  /// \param[in] name
+  ///   The command name, which should match one of the defined strings.
+  SagSpanCommand(const std::string& name);
+
+  /// \brief Destructor.
+  virtual ~SagSpanCommand();
+
+  /// \brief Creates a sag span from an xml node.
+  /// \param[in] node
+  ///   The xml node.
+  /// \param[out] span
+  ///   The sag span to populate.
+  /// \return Success status.
+  static bool CreateSpanFromXml(const wxXmlNode* node, SagSpan& span);
+
+  /// \brief Does the command.
+  /// \return If the action completes successfully.
+  virtual bool Do();
+
+  /// \brief Saves the sag span to an xml node.
+  /// \param[in] span
+  ///   The sag span.
+  /// \return An xml node for the sag span.
+  static wxXmlNode* SaveSpanToXml(const SagSpan& span);
+
+  /// \brief Undoes the command.
+  /// \return True if the action completes successfully.
+  virtual bool Undo();
+
+  /// \brief Gets the index.
+  /// \return The index.
+  int index() const;
+
+  /// \brief Gets the span xml node.
+  /// \return The span xml node.
+  const wxXmlNode* node_span() const;
+
+  /// \brief Sets the index.
+  /// \param[in] index
+  ///   The index.
+  void set_index(const int& index);
+
+  /// \brief Sets the span xml node.
+  /// \param[in] node_span
+  ///   The span xml node.
+  void set_node_span(const wxXmlNode* node_span);
+
+ private:
+  /// \brief Does the delete span command.
+  /// \return The success status.
+  bool DoDelete();
+
+  /// \brief Does the insert span command.
+  /// \param[in] node
+  ///   The xml node of the span.
+  /// \return The success status.
+  bool DoInsert(const wxXmlNode* node);
+
+  /// \brief Does the modify span command.
+  /// \param[in] node
+  ///   The xml node of the span.
+  /// \return The success status.
+  bool DoModify(const wxXmlNode* node);
+
+  /// \brief Does the move span down command.
+  /// \return The success status.
+  bool DoMoveDown();
+
+  /// \brief Does the move span up command.
+  /// \return The success status.
+  bool DoMoveUp();
+
+  /// \var doc_
+  ///   The document.
+  OnSagDoc* doc_;
+
+  /// \var index_
+  ///   The index to the spans list in the document. The command will be applied
+  ///   at this index.
+  int index_;
+
+  /// \var node_do_
+  ///   The span xml node that is committed to the document on a do operation.
+  ///   This command owns the node.
+  const wxXmlNode* node_do_;
+
+  /// \var node_undo_
+  ///   The span xml node that is committed to the document on an undo
+  ///   operation. It contains the state of the span in the document before any
+  /// edits occur. This command owns the node.
+  const wxXmlNode* node_undo_;
+};
+
+#endif  // OTLS_ONSAG_ONSAGDOCCOMMANDS_H_
---
+++ src/on_sag_doc_commands.cc
@@ -0,0 +1,242 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#include "on_sag_doc_commands.h"
+
+#include "wx/wx.h"
+
+#include "on_sag_app.h"
+#include "on_sag_doc.h"
+#include "sag_span_xml_handler.h"
+
+// These are the string definitions for the various command types.
+const std::string SagSpanCommand::kNameDelete = "Delete Span";
+const std::string SagSpanCommand::kNameInsert = "Insert Span";
+const std::string SagSpanCommand::kNameModify = "Modify Span";
+const std::string SagSpanCommand::kNameMoveDown = "Move Span Down";
+const std::string SagSpanCommand::kNameMoveUp = "Move Span Up";
+
+SagSpanCommand::SagSpanCommand(const std::string& name)
+    : wxCommand(true, name) {
+  doc_ = wxGetApp().GetDocument();
+  index_ = -1;
+  node_do_ = nullptr;
+  node_undo_ = nullptr;
+}
+
+SagSpanCommand::~SagSpanCommand() {
+  delete node_do_;
+  delete node_undo_;
+}
+
+bool SagSpanCommand::CreateSpanFromXml(const wxXmlNode* node, SagSpan& span) {
+  // clears span
+  span = SagSpan();
+
+  // parses node into span
+  const bool status_node = SagSpanXmlHandler::ParseNode(node, "", span);
+  if (status_node == false) {
+    wxString message =
+        SagSpanXmlHandler::FileAndLineNumber("", node)
+        + "Invalid span. Skipping.";
+    wxLogError(message);
+  }
+
+  return status_node;
+}
+
+bool SagSpanCommand::Do() {
+  bool status = false;
+
+  // clears undo node so it's ready to cache the existing state
+  if (node_undo_ != nullptr) {
+    delete node_undo_;
+  }
+
+  // selects based on command name
+  const std::string name = GetName();
+  if (name == kNameDelete) {
+    // caches span to xml and then does command
+    if (doc_->IsValidIndex(index_, false) == false) {
+      wxLogError("Invalid span index. Aborting command.");
+      return false;
+    }
+    const SagSpan& span = *std::next(doc_->spans().cbegin(), index_);
+    node_undo_ = SaveSpanToXml(span);
+    status = DoDelete();
+  } else if (name == kNameInsert) {
+    // does command
+    status = DoInsert(node_do_);
+  } else if (name == kNameModify) {
+    // caches span to xml and then does command
+    if (doc_->IsValidIndex(index_, false) == false) {
+      wxLogError("Invalid span index. Aborting command.");
+      return false;
+    }
+    const SagSpan& span = *std::next(doc_->spans().cbegin(), index_);
+    node_undo_ = SaveSpanToXml(span);
+    status = DoModify(node_do_);
+  } else if (name == kNameMoveDown) {
+    // does command
+    status = DoMoveDown();
+  } else if (name == kNameMoveUp) {
+    // does command
+    status = DoMoveUp();
+  } else {
+    status = false;
+
+    wxLogError("Invalid command. Aborting.");
+    return false;
+  }
+
+  // checks if command succeeded
+  if (status == true) {
+    // posts a view update
+    UpdateHint hint(UpdateHint::Type::kSpansEdit);
+    doc_->UpdateAllViews(nullptr, &hint);
+  } else {
+    // logs error
+    wxLogError("Do command failed.");
+  }
+
+  return status;
+}
+
+wxXmlNode* SagSpanCommand::SaveSpanToXml(const SagSpan& span) {
+  // copies span
+  SagSpan span_modified = span;
+
+  // gets unit system and returns xml node
+  units::UnitSystem units = wxGetApp().config()->units;
+  return SagSpanXmlHandler::CreateNode(span_modified, "", units);;
+}
+
+bool SagSpanCommand::Undo() {
+  bool status = false;
+
+  // selects based on command name
+  const std::string name = GetName();
+  if (name == kNameDelete) {
+    status = DoInsert(node_undo_);
+  } else if (name == kNameInsert) {
+    status = DoDelete();
+  } else if (name == kNameModify) {
+    status = DoModify(node_undo_);
+  } else if (name == kNameMoveDown) {
+    status = DoMoveUp();
+  } else if (name == kNameMoveUp) {
+    status = DoMoveDown();
+  } else {
+    status = false;
+
+    wxLogError("Invalid command. Aborting.");
+    return false;
+  }
+
+  // checks if command succeeded
+  if (status == true) {
+    // posts a view update
+    UpdateHint hint(UpdateHint::Type::kSpansEdit);
+    doc_->UpdateAllViews(nullptr, &hint);
+  } else {
+    // logs error
+    wxLogError("Undo command failed.");
+  }
+
+  return status;
+}
+
+int SagSpanCommand::index() const {
+  return index_;
+}
+
+const wxXmlNode* SagSpanCommand::node_span() const {
+  return node_do_;
+}
+
+void SagSpanCommand::set_index(const int& index) {
+  index_ = index;
+}
+
+void SagSpanCommand::set_node_span(const wxXmlNode* node_span) {
+  node_do_ = node_span;
+}
+
+bool SagSpanCommand::DoDelete() {
+  // checks index
+  if (doc_->IsValidIndex(index_, false) == false) {
+    wxLogError("Invalid index. Aborting delete command.");
+    return false;
+  }
+
+  // deletes from document
+  return doc_->DeleteSpan(index_);
+}
+
+bool SagSpanCommand::DoInsert(const wxXmlNode* node) {
+  // checks index
+  if (doc_->IsValidIndex(index_, true) == false) {
+    wxLogError("Invalid index. Aborting insert command.");
+    return false;
+  }
+
+  // builds span from xml node
+  SagSpan span;
+  CreateSpanFromXml(node, span);
+
+  // inserts span to document
+  return doc_->InsertSpan(index_, span);
+}
+
+bool SagSpanCommand::DoModify(const wxXmlNode* node) {
+  // checks index
+  if (doc_->IsValidIndex(index_, false) == false) {
+    wxLogError("Invalid index. Aborting modify command.");
+    return false;
+  }
+
+  // builds span from xml node
+  SagSpan span;
+  bool status = CreateSpanFromXml(node, span);
+  if (status == false) {
+    return false;
+  }
+
+  // modifies the document
+  return doc_->ModifySpan(index_, span);
+}
+
+bool SagSpanCommand::DoMoveDown() {
+  // checks index, and checks against the last valid index
+  const int kSizeSpans = doc_->spans().size();
+  if ((doc_->IsValidIndex(index_, false) == false)
+      || (index_ == kSizeSpans - 1)) {
+    wxLogError("Invalid index. Aborting move down command.");
+    return false;
+  }
+
+  // swaps the spans in the document
+  bool status = doc_->MoveSpan(index_, index_ + 2);
+
+  // increments the command index
+  index_ = index_ + 1;
+
+  return status;
+}
+
+bool SagSpanCommand::DoMoveUp() {
+  // checks index, and checks against the first valid index
+  if ((doc_->IsValidIndex(index_, false) == false)
+      || (index_ == 0)) {
+    wxLogError("Invalid index. Aborting move up command.");
+    return false;
+  }
+
+  // swaps the spans in the document
+  bool status = doc_->MoveSpan(index_, index_ - 1);
+
+  // decrements the command index
+  index_ = index_ - 1;
+
+  return status;
+}
