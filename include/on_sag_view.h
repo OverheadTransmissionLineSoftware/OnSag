0e09f22a3ec76ff1afca3509ebcf03ed771c52cc
bretzel12 <bretzel12@users.noreply.github.com>
2018-02-20 21:51:50 -0800

    OnSagView updated to handle the edit, results, and plot panes.

--- include/on_sag_view.h
+++ include/on_sag_view.h
@@ -6,6 +6,10 @@
 
 #include "wx/docview.h"
 
+#include "edit_pane.h"
+#include "profile_plot_pane.h"
+#include "results_pane.h"
+
 /// \par OVERVIEW
 ///
 /// This is the application view, which is responsible for displaying
@@ -59,6 +63,27 @@ class OnSagView : public wxView {
   /// This function is called by wxWidgets.
   virtual void OnUpdate(wxView *sender, wxObject *hint = nullptr);
 
+  /// \brief Gets the result index.
+  /// \return The result index.
+  const int index_result() const;
+
+  /// \brief Gets the edit pane.
+  /// \return The edit pane.
+  EditPane* pane_edit();
+
+  /// \brief Gets the profile plot pane.
+  /// \return The profile plot pane.
+  ProfilePlotPane* pane_plot();
+
+  /// \brief Gets the results pane.
+  /// \return The results pane.
+  ResultsPane* pane_results();
+
+  /// \brief Sets the selected analysis result.
+  /// \param[in] index
+  ///   The selected result index.
+  void set_index_result(const int& index);
+
   /// \brief Sets the render target.
   /// \param[in] target_render
   ///   The render target.
@@ -69,6 +94,23 @@ class OnSagView : public wxView {
   RenderTarget target_render() const;
 
  private:
+  /// \var index_result_
+  ///   The result index that is currently selected. If no result is displayed,
+  ///   set to -1.
+  int index_result_;
+
+  /// \var pane_edit_
+  ///   The edit pane.
+  EditPane* pane_edit_;
+
+  /// \var pane_plot_
+  ///   The plot pane.
+  ProfilePlotPane* pane_plot_;
+
+  /// \var pane_results_
+  ///   The results pane.
+  ResultsPane* pane_results_;
+
   /// \var target_render_
   ///   The render target.
   RenderTarget target_render_;
--- src/on_sag_view.cc
+++ src/on_sag_view.cc
@@ -4,6 +4,7 @@
 #include "on_sag_view.h"
 
 #include "appcommon/widgets/status_bar_log.h"
+#include "wx/cmdproc.h"
 
 #include "on_sag_app.h"
 
@@ -26,6 +27,11 @@ bool OnSagView::OnClose(bool WXUNUSED(delete_window)) {
   // saves AUI perspective
   wxGetApp().config()->perspective = manager->SavePerspective();
 
+  // detaches panes
+  manager->DetachPane(pane_results_);
+  manager->DetachPane(pane_edit_);
+  manager->DetachPane(pane_plot_);
+
   // adjusts log if the pane was docked
   wxAuiPaneInfo& info = manager->GetPane("Log");
   if (info.IsDocked() == true) {
@@ -35,6 +41,11 @@ bool OnSagView::OnClose(bool WXUNUSED(delete_window)) {
 
   // updates manager to show changes
   manager->Update();
+
+  // destroys panes
+  pane_edit_->Destroy();
+  pane_plot_->Destroy();
+  pane_results_->Destroy();
 
   // resets frame to document-less state
   OnSagFrame* frame = wxGetApp().frame();
@@ -60,6 +71,7 @@ bool OnSagView::OnCreate(wxDocument *doc, long flags) {
   }
 
   // initializes cached references
+  index_result_ = -1;
   target_render_ = RenderTarget::kScreen;
 
   // gets main application frame
@@ -69,16 +81,75 @@ bool OnSagView::OnCreate(wxDocument *doc, long flags) {
   SetFrame(frame);
   Activate(true);
 
-  /// \todo add details here
+  // gets aui manager
+  wxAuiManager* manager = wxAuiManager::GetManager(frame);
+
+  // creates AUI windows and adds to manager
+  wxAuiPaneInfo info;
+
+  info = wxAuiPaneInfo();
+  info.Name("Plot");
+  info.CenterPane();
+  pane_plot_ = new ProfilePlotPane(frame, this);
+  manager->AddPane(pane_plot_, info);
+
+  info = wxAuiPaneInfo();
+  info.Name("Results");
+  info.BestSize(300, 700);
+  info.Caption("Results");
+  info.CloseButton(false);
+  info.Floatable(false);
+  info.MinSize(150, 350);
+  info.Top();
+  pane_results_ = new ResultsPane(frame, this);
+  manager->AddPane(pane_results_, info);
+
+  info = wxAuiPaneInfo();
+  info.Name("Edit");
+  info.BestSize(300, 700);
+  info.Caption("Edit");
+  info.CloseButton(false);
+  info.Floatable(false);
+  info.Layer(1);
+  info.Left();
+  info.MinSize(150, 350);
+  pane_edit_ = new EditPane(frame, this);
+  manager->AddPane(pane_edit_, info);
+
+  // loads perspective and updates
+  std::string perspective = wxGetApp().config()->perspective;
+  if (perspective == "") {
+    manager->Update();
+  } else {
+    manager->LoadPerspective(wxGetApp().config()->perspective);
+  }
 
   // resets statusbar
   status_bar_log::SetText("Ready", 0);
+
+  // links command processor to edit menu
+  const int index_menu = frame->GetMenuBar()->FindMenu("Edit");
+  wxMenu* menu = frame->GetMenuBar()->GetMenu(index_menu);
+
+  wxCommandProcessor* processor = GetDocument()->GetCommandProcessor();
+  processor->SetEditMenu(menu);
 
   return true;
 }
 
 void OnSagView::OnDraw(wxDC *dc) {
-  /// \todo add details here
+  // caches the current background
+  // alters the background to white if view is rendering to print
+  const wxBrush brush = pane_plot_->background();
+  if (target_render_ == RenderTarget::kPrint) {
+    pane_plot_->set_background(*wxWHITE_BRUSH);
+  }
+
+  // draws the active plot pane
+  pane_plot_->RenderPlot(*dc);
+
+  // resets to the original background
+  pane_plot_->set_background(brush);
 }
 
 void OnSagView::OnUpdate(wxView* sender, wxObject* hint) {
@@ -88,10 +159,33 @@ void OnSagView::OnUpdate(wxView* sender, wxObject* hint) {
   // updates statusbar
   status_bar_log::PushText("Updating view", 0);
 
-  /// \todo add details here
+  // don't need to distinguish sender - all frames are grouped under one view
+  pane_edit_->Update(hint);
+  pane_results_->Update(hint);
+  pane_plot_->Update(hint);
 
   // resets status bar
   status_bar_log::PopText(0);
+}
+
+const int OnSagView::index_result() const {
+  return index_result_;
+}
+
+EditPane* OnSagView::pane_edit() {
+  return pane_edit_;
+}
+
+ProfilePlotPane* OnSagView::pane_plot() {
+  return pane_plot_;
+}
+
+ResultsPane* OnSagView::pane_results() {
+  return pane_results_;
+}
+
+void OnSagView::set_index_result(const int& index) {
+  index_result_ = index;
 }
 
 void OnSagView::set_target_render(
