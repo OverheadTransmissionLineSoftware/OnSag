33e9e06f0187643009344b9eb42e304dfbc72cc5
bretzel12 <bretzel12@users.noreply.github.com>
2018-03-12 20:10:41 -0700

    cpplint script updated.
    - AppCommon checks done first, then OnSag checks.
    - Filters added to suppress errors tied to using the wxWidgets
      framework. If these filters were not globally supressed, it would
      require a significant amount of // NOLINT comments.

--- check/run-cpplint.sh
+++ check/run-cpplint.sh
@@ -6,14 +6,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # defines settings
 PATH_CPPLINT="../external/styleguide/cpplint/cpplint.py"
 
-# generates a list of OnSag files and runs cpplint
-OPTIONS="--root="$DIR"/../include/ --extensions=h,cc --linelength=80 --filter=-legal/copyright"
-DIRS_SEARCH="$DIR/../include/ $DIR/../src/"
+# generates a list of AppCommon files and runs cpplint
+OPTIONS="--root="$DIR"/../external/AppCommon/include/ --extensions=h,cc --filter=-legal/copyright,-readability/utf8,-runtime/explicit,-runtime/int,-runtime/references,-runtime/string,-whitespace/line_length"
+DIRS_SEARCH="$DIR/../external/AppCommon/include/ $DIR/../external/AppCommon/src/"
 FILES=$(find $DIRS_SEARCH -type f \( -name "*.cc" -o -name "*.h" \) -print)
 $DIR/$PATH_CPPLINT $OPTIONS $FILES
 
-# generates a list of AppCommon files and runs cpplint
-OPTIONS="--root="$DIR"/../external/AppCommon/include/ --extensions=h,cc --linelength=80 --filter=-legal/copyright"
-DIRS_SEARCH="$DIR/../external/AppCommon/include/ $DIR/../external/AppCommon/src/"
+echo
+echo
+
+# generates a list of OnSag files and runs cpplint
+OPTIONS="--root="$DIR"/../include/ --extensions=h,cc --filter=-legal/copyright,-readability/utf8,-runtime/explicit,-runtime/int,-runtime/references,-runtime/string,-whitespace/line_length"
+DIRS_SEARCH="$DIR/../include/ $DIR/../src/"
 FILES=$(find $DIRS_SEARCH -type f \( -name "*.cc" -o -name "*.h" \) -print)
 $DIR/$PATH_CPPLINT $OPTIONS $FILES
